f4f13ceec307136addb834a8f54194b0f413e2fb
Federico Haag <federicohaag@outlook.com>
2025-10-04 23:11:59 +0000

    feat: Import hospitals module and export HOSPITALS constant

--- operatore-118/src/model/aggregates.tsx
+++ operatore-118/src/model/aggregates.tsx
@@ -1,4 +1,7 @@
-import type { Region } from "./types";
+import type { Hospital, Region } from "./types";
 import * as regions from './regions';
+import * as hospitals from './hospitals';
 
 export const REGIONS: Region[] = Object.values(regions);
+
+export const HOSPITALS: Hospital[] = Object.values(hospitals);
